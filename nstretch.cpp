f026291d31cf259447ddbac0d345403090f5b137
mschemmel <mschemmel@online.de>
2022-12-08 11:00:37 +0100

    care about last character(s) of N

--- nstretch.cpp
+++ nstretch.cpp
@@ -19,6 +19,12 @@ void readSequence(std::istream& input) {
     while(std::getline(input,line).good()){
       
       if(line[0] == '>'){
+          // was the last character of the previous sequence an 'N'?
+          if (found_start == true) {
+            std::cout << name << "\t" << start_position << "\t" << start_position << "\t" << '1' << std::endl;
+            found_start = false;
+            found_end = false;
+          }
           name = line.substr(1);
           position = 1;
      }
@@ -28,7 +34,7 @@ void readSequence(std::istream& input) {
                 const char currentChar = line[i];
                 // the current char is 'N'?
                 if(currentChar == 'N' || currentChar == 'n') {
-                  // do we have a start position already
+                  // do we have a start position already?
                   // if no, this is the start
                   if(found_start == false) {
                       start_position = position;
@@ -40,14 +46,14 @@ void readSequence(std::istream& input) {
                   // but we already had a start position
                   if(found_start == true) {
                       // so this has to be the end of the N stretch
-                      end_position = position;
+                      end_position = position - 1;
                       found_end = true;
                   }
                 }
                 // we do have both, start and end position
                 // we found the whole stretch -> print and clean up for next hit
                 if(found_start == true && found_end == true) {
-                  const unsigned int range = end_position - start_position;
+                  const unsigned int range = end_position == start_position ? 1 : (end_position + 1) - start_position;
                   std::cout << name << "\t" << start_position << "\t" << end_position << "\t" << range << std::endl;
                   found_start = false;
                   found_end = false;
